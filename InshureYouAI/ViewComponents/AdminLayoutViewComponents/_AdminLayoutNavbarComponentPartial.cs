45ce866855a4b3b308727845fb734ae704708440
isnuraina <nurane.ismayilzade19@gmail.com>
2025-10-17 15:03:40 +0400

    Navbar is ready

---
+++ InshureYouAI/ViewComponents/AdminLayoutViewComponents/_AdminLayoutNavbarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InshureYouAI.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutNavbarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ InshureYouAI/ViewComponents/AdminLayoutViewComponents/_AdminLayoutNavbarMenuComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InshureYouAI.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutNavbarMenuComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
